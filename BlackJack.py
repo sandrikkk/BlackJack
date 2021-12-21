de159256ff22211f5ac0110896d57ab6883c20b8
sandrikkk <94530708+sandrikkk@users.noreply.github.com>
2021-12-21 10:44:58 +0400

    Update BlackJack.py

--- BlackJack.py
+++ BlackJack.py
@@ -17,22 +17,22 @@ def black_jack(cards): #შემთხვევით კარტის ამ
 
 def shedareba(motamashis_qula, kompiuteris_qula): #ადარებს ქულებს
   if(motamashis_qula > 21 and kompiuteris_qula > 21): #თუ მოთამაშისა და კომპიუტერის ქულა აღემატება 21-ს
-    return "Tqven Samwuxarod Waaget" # აბრუნებს მოცემულ მნიშვნელობას
+    return "თქვენ სამწუხაროდ წააგეთ !" # აბრუნებს მოცემულ მნიშვნელობას
 
   if(motamashis_qula == kompiuteris_qula): #საყაიმო შეტყობინების გამოსატანად
-    return "Tamashi Yaimit Dasrulda" # აბრუნებს საყაიმო შეტყობინებას
+    return "თამაში ყაიმით დასრულდა" # აბრუნებს საყაიმო შეტყობინებას
   elif(kompiuteris_qula == "BlackJack"): #თუ კომპიუტერს ჰყავს Blackjack აბრუნდებს მე 9 ხაზის - ამ ფუნქციის შედეგს def black_jack(cards)
-    return "Tqven damarcxdit, Kompiuters Hyavs Blackjack" #გამოაქვს შესაბამისი შეტყობინება
+    return "თქვენ დამარცხდით კომპიუტერს ჰყავს Blackjack" #გამოაქვს შესაბამისი შეტყობინება
   elif(motamashis_qula == "BlackJack"): #თუ მოთამაშეს ჰყავს Blackjack აბრუნდებს მე 9 ხაზის - ამ ფუნქციის შედეგს def black_jack(cards)
-    return "Tqven gaimarjvet Blackjack-it" #გამოაქვს შესაბამისი შეტყობინება
+    return "თქვენ გაიმარჯვეთ Blackjack-ით !" #გამოაქვს შესაბამისი შეტყობინება
   elif (motamashis_qula > 21): #თუ მოთამაშის ქულა აღემატება 21-ს
-    return "Tqveni Shedegi agemateba 21 qulas, Tqven Damarcxdit" #გამოაქვს შესაბამისი შეტყობინება
+    return "თქვენი შედეგი აღემატება 21 ქულას , თქვენ დამარცხდით !" #გამოაქვს შესაბამისი შეტყობინება
   elif (kompiuteris_qula > 21): # თუ კომპიუტერის ქულა აღემატება 21 -ს
-    return "Kompiuteris qula Agemateba 21 - s, Tqven Gaimarjevt" #გამოაქვს შესაბამისი შეტყობინება
+    return "კომპიუტერის ქულა აღემატება 21 ქულას, თქვენ გაიმარჯვეთ !" #გამოაქვს შესაბამისი შეტყობინება
   elif (motamashis_qula > kompiuteris_qula): #თუ კომპიუტერის ქულა აღემატება მოთამაშის ქულას
-    return "Tqven moiget" #გამოაქვს შესაბამისი შეტყობინება
+    return "გილოცავთ, თქვენ მოიგეთ!" #გამოაქვს შესაბამისი შეტყობინება
   else: #წინააღმდეგ შემთხვევაში
-    return "Tqven waaget" #გამოაქვს შესაბამისი შეტყობინება
+    return "სამწუხაროდ დამარცხდით !" #გამოაქვს შესაბამისი შეტყობინება
 
 def tamashis_dawyeba(): #მთავარი მენიუ
   print(logo)#სვამს  from art import logo , ცვლად ლოგოს, რომელშიც დახატულია ეს ყველაფერი.
@@ -47,13 +47,13 @@ def tamashis_dawyeba(): #მთავარი მენიუ
   while not tamashis_statusi: # სანამ თამაშის სტატუსი ჭეშმარიტი არ გახდება მანამდე არ წვვეტს მუშაობას ციკლი (tamashis_statusi = False)
     motamashis_qula = black_jack(motamashis_karti) #სვამს მოცემულ ფუნქციაში ციფრებს, რომელი ფუნქციაც ამ ციფრებს ერთმანეთს უმატებს, ამ შემთხვევაში, მოთამაშის კარტს.
     kompiuteris_qula = black_jack(kompiuteris_karti)#სვამს მოცემულ ფუნქციაში ციფრებს, რომელი ფუნქციაც ამ ციფრებს ერთმანეთს უმატებს, ამ შემთხვევაში, კომპიუტერის კარტს.
-    print("Sheni kartia:", motamashis_karti, "Da jamshi = ", motamashis_qula) #ბეჭდავს მიღებულ შედეგებს
-    print("Kompiuteris kartia:", kompiuteris_karti, "Da jamshi = ", kompiuteris_qula) #ბეჭდავს მიღებულ შედეგებს
+    print("თქვენი კარტია:", motamashis_karti, "ჯამში = ", motamashis_qula) #ბეჭდავს მიღებულ შედეგებს
+    print("კომპიუტერის პირველი კარტია:", kompiuteris_karti[0]) #ბეჭდავს მიღებულ შედეგებს
 
     if motamashis_qula == "BlackJack" or kompiuteris_qula == "BlackJack" or motamashis_qula > 21: #ასრულებს ოპერაციებს
       tamashis_statusi = True #ციკლის ასრულებს თუ ეს პირობა შესრულდა
     else: # წინააღმდეგ შემთხვევაში ასრულებს ამ პირობას 56 ლაინზე
-      shetavazeba = input(str("Daachiret 'y' Gilaks Tu Gsurt kartis ageba Tu ara Daachiret 'n': "))
+      shetavazeba = input(str("დააჭირეთ 'y' ღილაკს თუ გსურთ კარტის აღება თუ არა დააჭირეთ 'n': "))
       if(shetavazeba == 'y'):  # თუ y დააჭირა მომხმარებელმა მაშინ 58 ლაინზე რაცაა იმას ასრულებს
         motamashis_karti.append(kartis_archeva()) #სვამს მესამე კარტს
       else:
@@ -63,9 +63,10 @@ def tamashis_dawyeba(): #მთავარი მენიუ
   while kompiuteris_qula != 0 and kompiuteris_qula < 17: #სანამ 0-ს არ უდრის და სანამ 17 ზე ნაკლებია
     kompiuteris_karti.append(kartis_archeva()) #ამატებს კარტს
     kompiuteris_qula = black_jack(kompiuteris_karti) # ითვლის რაოდენობას
-  print("Motamashis Saboloo Kartia:", motamashis_karti, "saboloo shedegia: ", motamashis_qula) # გამოაქვს შედეგები
-  print("Kompiuteris Saboloo Kartia:", kompiuteris_karti, "saboloo shedegia: ", kompiuteris_qula) # გამოაქვს შედეგები
+  print("მოთამაშის საბოლოო კარტია:", motamashis_karti, "საბოლოო შედეგია: ", motamashis_qula) # გამოაქვს შედეგები
+  print("კომპიუტერის საბოლოო კარტია:", kompiuteris_karti, "საბოლოო შედეგია: ", kompiuteris_qula) # გამოაქვს შედეგები
   print(shedareba(motamashis_qula,kompiuteris_qula)) #ადარებს მე 18 ხაზზე განთავსებული ფუნქიციის წინაპირობებს
 
-while input("Gindat Black-Jack - is tamashi? Akrifet An 'y' an 'n'") == 'y': #გვეკითხება თუ გვინდა თამაშის დაწყება
+while input("დავიწყოთ Black-Jack - ის თამაში? აკრიფეთ 'y' ან 'n'") == 'y': #გვეკითხება თუ გვინდა თამაშის დაწყება
   tamashis_dawyeba() # იძახებს თამაშის მთავარ ფუნქციას სადაც გაერთიანებულია ყველა ფუნქცია!
+
